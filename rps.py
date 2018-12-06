e4bc3601148ac0ad331356edce017b42984a2d32
s-gabor <ing.gabor@yahoo.com>
2018-12-06 17:16:26 +0200

    add render_template

--- rps.py
+++ rps.py
@@ -1,6 +1,6 @@
 # rps.py
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Rock, Paper, Scissors'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
