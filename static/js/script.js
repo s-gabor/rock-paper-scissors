e664d5a8450a94b9816bf32efe72fd96742fe771
s-gabor <ing.gabor@yahoo.com>
2018-12-06 17:11:22 +0200

    add interactive UI

---
+++ static/js/script.js
@@ -0,0 +1,48 @@
+document.getElementById('rock').addEventListener('click', function() {
+  play('rock');
+});
+
+document.getElementById('paper').addEventListener('click', function() {
+  play('paper');
+});
+
+document.getElementById('scissors').addEventListener('click', function() {
+  play('scissors');
+});
+
+
+function randomChoice(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+
+function getWinner(computerChoice, userChoice) {
+  if (computerChoice === "rock" && userChoice === "paper") {
+    return "User";
+  } else if (computerChoice === "rock" && userChoice === "scissors") {
+    return "Computer";
+  } else if (computerChoice === "paper" && userChoice === "rock") {
+    return "Computer";
+  } else if (computerChoice === "paper" && userChoice === "scissors") {
+    return "User";
+  } else if (computerChoice === "scissors" && userChoice === "rock") {
+    return "User";
+  } else if (computerChoice === "scissors" && userChoice === "paper") {
+    return "Computer";
+  } else {
+    return "It's a tie. Nobody";
+  }
+}
+
+
+function play(userChoice) {
+  // Determine the winner
+  var possibilities = ["rock", "paper", "scissors"];
+  var computerChoice = randomChoice(possibilities);
+  var winner = getWinner(computerChoice, userChoice);
+
+  // Display results
+  document.querySelector('.computer').innerHTML = 'Computer\'s choice: <strong>' + computerChoice + '</strong>';
+  document.querySelector('.user').innerHTML = 'User\'s choice: <strong>' + userChoice + '</strong>';
+  document.querySelector('.result').textContent = winner + ' wins!';
+}
